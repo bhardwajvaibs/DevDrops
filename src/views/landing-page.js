4d0559bdca1aed965bbbd0b40472a6db40412331
Vaibhav Bhardwaj <bhardwaj.vaibs@gmail.com>
2025-07-17 23:08:02 +0530

    rearrange

--- src/views/landing-page.js
+++ src/views/landing-page.js
@@ -69,101 +69,10 @@ const LandingPage = (props) => {
             }
              showButton={true}
              repoUrl="https://github.com/bhardwajvaibs/todo"
-  techStack={["React Native ", "Firebase", "MongoDB", "Render","Javascript","Visual Studio Code","Android Studio"]}
+  techStack={["Kotlin","React Native ", "Firebase", "MongoDB", "Render","Javascript","Visual Studio Code","Android Studio"]}
              apkUrl="https://github.com/bhardwajvaibs/todo/releases/download/v1.0.0/app-release.apk"
           // rootClassName="place-cardroot-class-name"
           ></PlaceCard>
-          <PlaceCard
-            city="Vibe"
-            text={
-              <Fragment>
-                <span className="landing-page-text24">
-                  <span>
-                    Experience instant communication with our Real-Time Chat
-                    App, designed to keep you connected — anytime, anywhere.
-                  </span>
-                  <br></br>
-                  <br></br>
-                  <span>📡 Real-time messaging with instant updates.</span>
-                  <br></br>
-                  <span>🧑‍🤝‍🧑 One-on-one and group chats.</span>
-                  <br></br>
-                  <span>🔔 Push notifications for new messages.</span>
-                  <br></br>
-                  <span>✨ Clean and responsive UI.</span>
-                  <br></br>
-                  <span>🔒 Secure &amp; reliable communication .</span>
-                </span>
-              </Fragment>
-            }
-             showButton={true}
-               repoUrl="https://github.com/bhardwajvaibs/Vibe"
-  techStack={["React Native ", "Firebase", "MongoDB", "Render","Javascript", "Express.js", "Node.js","Amazon AWS ","Visual Studio Code","Android Studio"]}
-          apkUrl="https://github.com/bhardwajvaibs/Vibe/releases/download/v1.0.0/app-release.apk" 
-          ></PlaceCard>
-          <PlaceCard
-            city="Cure"
-            text={
-              <Fragment>
-                <span className="landing-page-text37">
-                  <span>
-                    Connect with the right doctor in real time. This app lets
-                    patients find doctors based on their health issues and start
-                    a private chat instantly.
-                  </span>
-                  <br></br>
-                  <br></br>
-                  <span>🔍 Search doctors by symptoms or specialization.</span>
-                  <br></br>
-                  <span>💬 Real-time chat for quick consultations.</span>
-                  <br></br>
-                  <span>🛡️ Secure and easy to use.</span>
-                  <br></br>
-                  <br></br>
-                  <span>
-                    Healthcare made simple, fast, and accessible — right from
-                    your phone.
-                  </span>
-                </span>
-              </Fragment>
-            }
-              repoUrl="https://github.com/bhardwajvaibs/cure"
-  techStack={["React Native ",  "MongoDB", "Render","Javascript","OpenRouter API","Express.js","Node.js","Amazon AWS "]}
-             showButton={false}
-          ></PlaceCard>
-         <PlaceCard
-            city="Cropify"
-            text={
-              <Fragment>
-                <span className="landing-page-text49">
-                  <span>
-                    Get the best crop for your land in seconds! This smart app
-                    recommends the most suitable crop based on 7 key conditions
-                    entered by the user.
-                  </span>
-                  <br></br>
-                  <br></br>
-                  <span>
-                    ✅ Enter soil type, pH, rainfall, temperature &amp; more.
-                  </span>
-                  <br></br>
-                  <span>🌱 Get the best crop suggestion instantly.</span>
-                  <br></br>
-                  <span>📊 Based on real data and smart algorithms.</span>
-                  <br></br>
-                  <br></br>
-                  <span>
-                    Farming made smarter, with the right crop at the right time.
-                  </span>
-                </span>
-              </Fragment>
-            }
-             showButton={true}
-               repoUrl="https://github.com/bhardwajvaibs/Cropify"
-  techStack={["React Native ","Javascript","Machine Learning","TensorFlow","Android Studio","Visual Code Studio"]}
-             apkUrl="https://github.com/bhardwajvaibs/Cropify/releases/download/v1.0.0/app-release.apk"
-          ></PlaceCard>
-         
           <PlaceCard
             city="Weather App"
             text={
@@ -199,6 +108,96 @@ const LandingPage = (props) => {
               repoUrl="https://github.com/bhardwajvaibs/weatherapplication"
    
             rootClassName="place-cardroot-class-name1"
+          ></PlaceCard>
+          <PlaceCard
+            city="Vibe"
+            text={
+              <Fragment>
+                <span className="landing-page-text24">
+                  <span>
+                    Experience instant communication with our Real-Time Chat
+                    App, designed to keep you connected — anytime, anywhere.
+                  </span>
+                  <br></br>
+                  <br></br>
+                  <span>📡 Real-time messaging with instant updates.</span>
+                  <br></br>
+                  <span>🧑‍🤝‍🧑 One-on-one and group chats.</span>
+                  <br></br>
+                  <span>🔔 Push notifications for new messages.</span>
+                  <br></br>
+                  <span>✨ Clean and responsive UI.</span>
+                  <br></br>
+                  <span>🔒 Secure &amp; reliable communication .</span>
+                </span>
+              </Fragment>
+            }
+             showButton={true}
+               repoUrl="https://github.com/bhardwajvaibs/Vibe"
+  techStack={["React Native ", "Firebase", "MongoDB", "Render","Javascript", "Express.js", "Node.js","Amazon AWS ","Visual Studio Code","Android Studio"]}
+          apkUrl="https://github.com/bhardwajvaibs/Vibe/releases/download/v1.0.0/app-release.apk" 
+          ></PlaceCard>
+          <PlaceCard
+            city="Cropify"
+            text={
+              <Fragment>
+                <span className="landing-page-text49">
+                  <span>
+                    Get the best crop for your land in seconds! This smart app
+                    recommends the most suitable crop based on 7 key conditions
+                    entered by the user.
+                  </span>
+                  <br></br>
+                  <br></br>
+                  <span>
+                    ✅ Enter soil type, pH, rainfall, temperature &amp; more.
+                  </span>
+                  <br></br>
+                  <span>🌱 Get the best crop suggestion instantly.</span>
+                  <br></br>
+                  <span>📊 Based on real data and smart algorithms.</span>
+                  <br></br>
+                  <br></br>
+                  <span>
+                    Farming made smarter, with the right crop at the right time.
+                  </span>
+                </span>
+              </Fragment>
+            }
+             showButton={true}
+               repoUrl="https://github.com/bhardwajvaibs/Cropify"
+  techStack={["React Native ","Javascript","Machine Learning","TensorFlow"]}
+             apkUrl="https://github.com/bhardwajvaibs/Cropify/releases/download/v1.0.0/app-release.apk"
+          ></PlaceCard>
+          <PlaceCard
+            city="Cure"
+            text={
+              <Fragment>
+                <span className="landing-page-text37">
+                  <span>
+                    Connect with the right doctor in real time. This app lets
+                    patients find doctors based on their health issues and start
+                    a private chat instantly.
+                  </span>
+                  <br></br>
+                  <br></br>
+                  <span>🔍 Search doctors by symptoms or specialization.</span>
+                  <br></br>
+                  <span>💬 Real-time chat for quick consultations.</span>
+                  <br></br>
+                  <span>🛡️ Secure and easy to use.</span>
+                  <br></br>
+                  <br></br>
+                  <span>
+                    Healthcare made simple, fast, and accessible — right from
+                    your phone.
+                  </span>
+                </span>
+              </Fragment>
+            }
+              repoUrl="https://github.com/bhardwajvaibs/cure"
+  techStack={["React Native ",  "MongoDB", "Render","Javascript","OpenRouter API","Express.js","Node.js","Amazon AWS "]}
+             showButton={false}
           ></PlaceCard>
           <PlaceCard
             city="Upcoming ..."
